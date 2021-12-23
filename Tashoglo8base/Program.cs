25526f414964b46cdf25d1b679906b94808176f4
Евгений Ташогло <tashoglo00@mail.ru>
2021-12-23 22:27:35 +0200

    Добавьте файлы проекта.

---
+++ Tashoglo8base/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tashoglo8base
+{
+    class Program
+    {
+        static void Main()                 
+        {
+             //1
+                Console.WriteLine("Введите время:");
+                TimeSpan.TryParse(Console.ReadLine(), out TimeSpan time);
+                Console.WriteLine($"Часы - {time.Hours}\nМинуты - {time.Minutes}\nСекунды - {time.Seconds}\nМиллисекунды - {time.Milliseconds}");                    
+                Console.Write("Введите время будильника: ");
+            //2
+                TimeSpan timeNow = DateTime.Now.TimeOfDay;
+                Console.WriteLine(timeNow >= time ? $"Будильник зазвенит через - {TimeSpan.FromHours(24).Subtract((timeNow.Subtract(time))).ToString(@"hh\:mm\:ss")}"
+                : $"Будильник зазвенит через - {timeNow.Subtract(time).ToString(@"hh\:mm\:ss")}");           
+        }        
+    }
+}
